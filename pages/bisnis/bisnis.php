e9a593168e5874ae062f8a9fe5268f54b92914fc
wali98-rgb <wyyuddin@gmail.com>
2023-12-23 19:50:09 +0700

    add: dropdown list

--- pages/bisnis/bisnis.php
+++ pages/bisnis/bisnis.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../../plugin/css/bisnis.css">
     <link rel="stylesheet" href="../../plugin/css/Db.css">
     <link rel="stylesheet" href="../../plugin/css/footer.css">
-    <link rel="stylesheet" href="../../plugin/css/navbar.css">
+    <link rel="stylesheet" href="../../plugin/css/Nav.css">
     <link rel="stylesheet" href="../../plugin/css/pry.css">
     
     <!-- My Icon Bootstrap -->
@@ -56,7 +56,13 @@
                 <!-- Nav Start -->
                     <div class="navbars-nav">
                         <a href="../../index.php">BERANDA</a>
-                        <a href="#">TENTANG KAMI</a>
+                        <div class="drop">
+                            <a href="#">TENTANG KAMI <i class="bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="../tentang/tentang-kami.php">Tentang Kami</a></li>
+                                <li><a href="../tentang/galeri.php">Galeri</a></li>
+                            </ul>
+                        </div>
                         <a href="../proyek/proyek.php">PROYEK</a>
                         <a class="aktif" href="bisnis.php">BISNIS KAMI</a>
                         <a href="../artikel/artikel.php">ARTIKEL</a>
--- pages/kontak/kontak.php
+++ pages/kontak/kontak.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="../../plugin/css/Db.css">
     <link rel="stylesheet" href="../../plugin/css/Footer.css">
     <link rel="stylesheet" href="../../plugin/css/kontak.css">
-    <link rel="stylesheet" href="../../plugin/css/Navbar.css">
+    <link rel="stylesheet" href="../../plugin/css/Nav.css">
     <link rel="stylesheet" href="../../plugin/css/pry.css">
     
     <!-- My Icon Bootstrap -->
@@ -57,7 +57,13 @@
                 <!-- Nav Start -->
                     <div class="navbars-nav">
                         <a href="../../index.php">BERANDA</a>
-                        <a href="#">TENTANG KAMI</a>
+                        <div class="drop">
+                            <a href="#">TENTANG KAMI <i class="bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="../tentang/tentang-kami.php">Tentang Kami</a></li>
+                                <li><a href="../tentang/galeri.php">Galeri</a></li>
+                            </ul>
+                        </div>
                         <a href="../proyek/proyek.php">PROYEK</a>
                         <a href="../bisnis/bisnis.php">BISNIS KAMI</a>
                         <a href="../artikel/artikel.php">ARTIKEL</a>
