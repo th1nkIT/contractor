0e93fe76b38dfb554b34d749a8b233011e8791e2
wali98-rgb <wyyuddin@gmail.com>
2024-01-26 09:49:32 +0700

    update: Dashboard Page (Jumbotron & Pertanyaan)

--- pages/bisnis/show_bisnis.php
+++ pages/bisnis/show_bisnis.php
@@ -33,7 +33,7 @@ $stmtAllCategory->close();
     <link rel="stylesheet" href="/plugin/css/App.css">
     <link rel="stylesheet" href="/plugin/css/Artikel.css">
     <link rel="stylesheet" href="/plugin/css/Bisnis.css">
-    <link rel="stylesheet" href="/plugin/css/Dashboard.css">
+    <link rel="stylesheet" href="/plugin/css/Dashboards.css">
     <link rel="stylesheet" href="/plugin/css/Footer.css">
     <link rel="stylesheet" href="/plugin/css/nv.css">
     <link rel="stylesheet" href="/plugin/css/Pro.css">
